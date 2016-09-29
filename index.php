c03da9301bab3f12962a29a2ef662b39bb6779ff
intetec <projetoBrasilComCiencia@gmail.com>
2016-09-29 01:18:25 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -1,11 +1,94 @@
+<!DOCTYPE HTML PUBLIC "- // W3C // DTD HTML 4.01 Transitional // EN"
+   "Http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <title>projetoBrasilComCiencia</title>
 </head>
 
-<body>
+	<body>
 
-<h1>Olá Mundo</h1>
+  <header>
+    <!-- header content goes in here -->
+    <svg height="130" width="500">
+	  	<defs>
+	    	<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+	     		<stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+	      		<stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+	    	</linearGradient>
+	  	</defs>
+	  	<ellipse cx="100" cy="70" rx="85" ry="55" fill="url(#grad1)" />
+	  		<text fill="#ffffff" font-size="45" font-family="Verdana" x="50" y="86">PBCC</text>
+	  			Sorry, your browser does not support inline SVG.
+	</svg>
+    <h1>Wake up sheeple!</h1>
+	  <p><a href="news.html">News</a> -
+	     <a href="blog.html">Blog</a> -
+	     <a href="forums.html">Forums</a></p>
+	  <p>Last Modified: <time>2009-04-01</time></p>
+  	<nav>
+	   	<h1>Navigation</h1>
+		   <ul>
+		    <li><a href="articles.html">Index of all articles</a></li>
+		    <li><a href="today.html">Things sheeple need to wake up for today</a></li>
+		    <li><a href="successes.html">Sheeple we have managed to wake</a></li>
+		   </ul>
+	  	</nav>
+  </header>
+
+  <nav>
+    <!-- navigation menu goes in here -->
+  </nav>
+
+  <section id="sidebar1">
+    <!-- sidebar content goes in here -->
+  </section>
+
+  	<section id="main">
+	    <article>
+	      <!-- first blog post content goes here -->
+	      <section id="introduction">
+			  </section>
+			  
+			  <section id="content">
+			  </section>
+			  
+			  <section id="summary">
+			  </section>
+	    </article>
+	    
+	    <article>
+	      <!-- second blog post content goes here -->
+		      <section id="introduction">
+			  </section>
+			  
+			  <section id="content">
+			  </section>
+			  
+			  <section id="summary">
+			  </section>
+	    </article>
+	    
+	    <article>
+	      <!-- third blog post content goes here -->
+		      <section id="introduction">
+			  </section>
+			  
+			  <section id="content">
+			  </section>
+			  
+			  <section id="summary">
+			  </section>
+	    </article>
+	</section>
+
+  <aside>
+    <!-- aside content goes in here -->
+  </aside>
+
+  <footer>
+    <!-- footer content goes in here -->
+  </footer>
 
 </body>
+
 </html>
