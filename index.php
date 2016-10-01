b284f7fb543e3e2a8ed4a35ac8a4d7909962189d
intetec <projetoBrasilComCiencia@gmail.com>
2016-10-01 02:40:59 -0300

    segunda atualização

--- index.php
+++ index.php
@@ -3,25 +3,108 @@
 <html>
 <head>
 <title>projetoBrasilComCiencia</title>
-<link href="../assets/css/bootstrap.css" rel="stylesheet">
 <link rel="stylesheet" href="style.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="style.css">
+
 </head>
 
 	<body>
 
   <header>
-    <!-- header content goes in here -->
-    <svg height="130" width="500">
-	  	<defs>
-	    	<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-	     		<stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
-	      		<stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
-	    	</linearGradient>
-	  	</defs>
-	  	<ellipse cx="100" cy="70" rx="85" ry="55" fill="url(#grad1)" />
-	  		<text fill="#ffffff" font-size="45" font-family="Verdana" x="50" y="86">PBCC</text>
-	  			Sorry, your browser does not support inline SVG.
+  	<div class="jumbotron text-center">
+  		<img src="Canvas/Sheeple fools who believe the mainstream news has no agenda but to report the truth.jpg" alt="pensamentos" />
+  		<h1>
+		<svg height="120" width="200">
+	  <defs>
+	    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+	      <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+	      <stop offset="100%" style="stop-color:rgb(39,214,180);stop-opacity:1" />
+	    </linearGradient>
+	  </defs>
+	  <ellipse cx="105" cy="60" rx="95" ry="55" fill="url(#grad1)" />
+	  <text fill="black" font-size="60" font-family="Verdana" x="35" y="84">PBCC</text>
+	  Sorry, your browser does not support inline SVG.
 	</svg>
+  		projetoBrasilComCiência</h1>
+  			<p>A humanidade caminha em fila rumo ao desconhecido !</p>
+  			<!-- Trigger the modal with a button -->
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Quer Saber Mais ?</button>
+
+		<!-- Modal -->
+		<div id="myModal" class="modal fade" role="dialog">
+		    <div class="modal-dialog">
+
+		    <!-- Modal content-->
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal">&times;</button>
+			        <h4 class="modal-title">Saiba Mais</h4>
+			      </div>
+			      <div class="modal-body">
+			        <p><a href="#">intetec.info</a>Tecnolgia de Informação</p>
+			        <div class="alert alert-success">
+  						<strong>Parabéns!</strong> A curiosidade leva voce a novos caminhos.
+					</div>
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+			      </div>
+		    </div>
+
+		 	</div>
+		</div> 
+	</div>
+    <!-- header content goes in here -->
+    <div class="well well-sm"><img src="Canvas/Chara04.png" alt=""><h2>Bem Vindos A Uma Nova Visão !</h2></div>
+		<div class="well well-lg"><img src="Canvas/167409.jpg" alt=""></div>
+    <svg height="120" width="200">
+	  <defs>
+	    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+	      <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+	      <stop offset="100%" style="stop-color:rgb(39,214,180);stop-opacity:1" />
+	    </linearGradient>
+	  </defs>
+	  <ellipse cx="105" cy="60" rx="95" ry="55" fill="url(#grad1)" />
+	  <text fill="black" font-size="60" font-family="Verdana" x="35" y="84">PBCC</text>
+	  Sorry, your browser does not support inline SVG.
+	</svg>
+  		<h1>projetoBrasilComCiência</h1>
+  			<p>Uma nova maneira de ver o nosso mundo !</p>
+  			<!-- Trigger the modal with a button -->
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Saiba Mais</button>
+
+		<!-- Modal -->
+		<div id="myModal" class="modal fade" role="dialog">
+		    <div class="modal-dialog">
+
+		    <!-- Modal content-->
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal">&times;</button>
+			        <h4 class="modal-title">Modal Header</h4>
+			      </div>
+			      <div class="modal-body">
+			        <p>Some text in the modal.</p>
+			        <div class="alert alert-success">
+  						<strong>Success!</strong> Indicates a successful or positive action.
+					</div>
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			      </div>
+		    </div>
+
+		 	</div>
+		</div> 
     <h1>Wake up sheeple!</h1>
 	  <p><a href="news.html">News</a> -
 	     <a href="blog.html">Blog</a> -
